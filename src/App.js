ee67310d89b50b4205b26904c33df36bd26babfb
JannatJameel <j_annat@outlook.com>
2021-01-24 11:58:36 +0300

    wooow

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import CookieList from "./components/ProductList";
 import Home from "./components/Home";
 import { ThemeProvider } from "styled-components";
 
+//States
+import {useState} from "react";
+
 const theme = {
   light: {
     mainColor: "#242424", // main font color
@@ -22,10 +25,19 @@ const theme = {
 };
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+  const themeSwitch = () => {
+    if(currentTheme === "light"){
+      setCurrentTheme("dark");
+    }else{
+      setCurrentTheme("light");
+    };
+  }; 
+
   return (
-    <ThemeProvider theme={theme.light}>
+    <ThemeProvider theme={currentTheme === "light"? theme.light : theme.dark}>
       <GlobalStyle />
-      <ThemeButton onClick={() => alert("I do nothing..")}>
+      <ThemeButton onClick={themeSwitch} theme>
         Dark Theme
       </ThemeButton>
       <Home />
